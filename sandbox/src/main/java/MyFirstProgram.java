251fba8effe6c5bce2542caeacaa6289cd0d983d
ameliepullen89 <102325015+ameliepullen89@users.noreply.github.com>
2023-04-26 18:50:58 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,10 +1,35 @@
 public class MyFirstProgram {
-	
-public static void main(String[] args){
-	System.out.println("Hello, world! How are you?");
-	
+
+  public static void main(String[] args) {
+    hello("Alice");
+    hello("guys");
+    hello("world");
+
+    double l = 5.5;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+    double a = 7.5;
+    double b = 8.9;
+    System.out.println("Площадь прямоугольника со сторонами  " + a + " и " + b + " равна " + areap(a, b));
+
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "! How are you?");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+
+public static double areap(double a, double b){
+    return a * b;
+
 }
 
 
 }
+
 
