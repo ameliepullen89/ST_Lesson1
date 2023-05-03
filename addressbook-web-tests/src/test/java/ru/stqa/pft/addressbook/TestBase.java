7028f32571f8232ee46576e1b9d3db5633c86932
ameliepullen89 <102325015+ameliepullen89@users.noreply.github.com>
2023-05-04 01:07:34 +0300

    Удален isAlertPresent из всех тестов и базового класса

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -69,14 +68,6 @@ public class TestBase {
     wd.findElement(By.linkText("Logout")).click();
   }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   protected void submitContactCreation() {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
