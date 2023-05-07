0ccee7804fb25914121969c8f7539c7c460761e0
ameliepullen89 <102325015+ameliepullen89@users.noreply.github.com>
2023-05-07 17:37:24 +0300

    Исправлен тест DistanceTest

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
@@ -14,24 +14,16 @@ public class DistanceTest {
 
   @Test
   public void testDistance2()     {
-    Point p1 = new Point(6, 7);
-    Point p2 = new Point(4, 11);
-    Assert.assertEquals(p1.distance(p2), 4.48);
+    Point p1 = new Point(-1, 21);
+    Point p2 = new Point(16, 5);
+    Assert.assertEquals(p1.distance(p2), 23.345235059857504);
   }
 
   @Test
   public void testDistance3()     {
-    Point p1 = new Point(6, 7);
-    Point p2 = new Point(4, 11);
-    Assert.assertEquals(p1.distance(p2), 4.47213595499957);
+    Point p1 = new Point(3, -4);
+    Point p2 = new Point(2, 6);
+    Assert.assertEquals(p1.distance(p2), 10.04987562112089);
   }
-
-  @Test
-  public void testDistance4()     {
-    Point p1 = new Point(6, 7);
-    Point p2 = new Point(4, 11);
-    Assert.assertEquals(p1.distance(p2), -5);
-  }
-
 
 }
