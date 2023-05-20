d6336c4c790d0c10f7304690b45a7560e5430a44
ameliepullen89 <102325015+ameliepullen89@users.noreply.github.com>
2023-05-20 20:18:48 +0300

    Добавлена проверка и обеспечение предусловий для удаления и редактирования группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -49,4 +49,15 @@ public class GroupHelper extends HelperBase  {
   public void submitGroupModification() {
     click(By.name("update"));
   }
+
+  public void createGroup(GroupData group) throws InterruptedException {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
