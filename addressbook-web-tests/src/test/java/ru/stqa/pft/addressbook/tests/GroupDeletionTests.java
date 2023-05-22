e06dec4a4c422edb5161d5b3c17b356152e514b9
ameliepullen89 <102325015+ameliepullen89@users.noreply.github.com>
2023-05-22 19:54:57 +0300

    Для метода selectGroup добавлена возможность выбирать группу по индексу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -14,7 +14,7 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getNavigationHelper().gotoGroupPage();
     int after = app.getGroupHelper().getGroupCount();
