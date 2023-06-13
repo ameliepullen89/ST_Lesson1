d7e9008363d23fa9c1d92df772341914248dcf88
ameliepullen89 <102325015+ameliepullen89@users.noreply.github.com>
2023-06-14 00:14:50 +0300

    Лекция 6.10. Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,9 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class ApplicationManager {
+  private final Properties properties;
   public WebDriver wd;
 
 
@@ -19,21 +26,24 @@ public class ApplicationManager {
   private String browser;
 
   public ApplicationManager(String browser) {
-
     this.browser = browser;
+    properties = new Properties();
+
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target","local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     if (browser.equals(Browser.CHROME.browserName())) { wd = new ChromeDriver();}
       else if (browser.equals(Browser.FIREFOX.browserName())) { wd = new FirefoxDriver();}
       else if (browser.equals(Browser.SAFARI.browserName())) { wd = new SafariDriver();};
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    wd.get("http://localhost/addressbook/index.php");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
 
